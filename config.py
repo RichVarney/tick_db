fb61306223ea25542337f0edaeda19af19b971f8
RichVarney <35279192+RichVarney@users.noreply.github.com>
2023-04-27 16:57:13 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Make sure to edit this configuration file with your database connection details
+# and Alpha Vantage API key
+DB_USER = '********'
+DB_PASS = ''********''
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'tick_db'
+APIKEY = 'alpha_vantage_apikey'
